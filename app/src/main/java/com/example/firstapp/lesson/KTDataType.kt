2a7dd0befde6a573e5b4c76300f931fa3898466c
Regina Zhang <reginazhang@Reginas-MacBook-Pro-2.local>
2022-03-30 23:26:41 -0700

    chp2

---
+++ app/src/main/java/com/example/firstapp/lesson/KTDataType.kt
@@ -0,0 +1,18 @@
+package com.example.firstapp.lesson
+
+fun main() {
+    //1. declare a datatype
+    //default data type is int, if exceed int range,
+    val number = 100 //editor
+    val number2:Int = 121
+    val bigNumber = 80000000000
+    val longNumber = 20L
+    val byteNumber:Byte = 1
+
+    // default: Double
+    //float only keep 6 digits after point
+    val doubleNumber = 3.12345678
+    val floatNumber = 3.12345678F
+    println("floatNumber: " + floatNumber)
+    println("doubleNumber: " + doubleNumber)
+}
